aaad8e0d9c10a22d780d8b9de12c5b971fe7331f
Rohit Jangid <73385164+rohitjangid666@users.noreply.github.com>
2024-10-13 20:36:22 +0530

    integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import FileUploadForm from '@/components/FileUploadForm';
+
 export default function Home() {
-  return <div className='min-h-screen p-12'></div>;
+  return (
+    <div className='min-h-screen p-12'>
+      <FileUploadForm />
+    </div>
+  );
 }
